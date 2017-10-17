cdb380401de4ff2ce92f9452dbf8952dfe55917b
Fabio Tudone <fabio_tudone@bmc.com>
2017-10-17 15:38:41 +0300

    :arrow_down: Downgrade to Spring Boot 1.3.x with latest MongoDB V2 Java driver

--- src/test/java/works/weave/socks/cart/repositories/ITCartRepository.java
+++ src/test/java/works/weave/socks/cart/repositories/ITCartRepository.java
@@ -5,14 +5,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import works.weave.socks.cart.entities.Cart;
 
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @EnableAutoConfiguration
 public class ITCartRepository {
     @Autowired
